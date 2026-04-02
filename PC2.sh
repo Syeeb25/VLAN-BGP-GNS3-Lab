3afee341718733948c95f05a6f670957b3178997
Syeeb25 <malliksyeeb@gmail.com>
2026-04-02 15:16:59 +0530

    Adding config for Pc2

---
+++ PC2.sh
@@ -0,0 +1,10 @@
+PC2> ip 192.168.1.2/24 192.168.1.1
+Checking for duplicate address...
+PC2 : 192.168.1.2 255.255.255.0 gateway 192.168.1.1
+
+PC2> ping 40.0.0.2
+
+84 bytes from 40.0.0.2 icmp_seq=1 ttl=63 time=29.237 ms
+84 bytes from 40.0.0.2 icmp_seq=2 ttl=63 time=17.050 ms
+84 bytes from 40.0.0.2 icmp_seq=3 ttl=63 time=18.556 ms
+84 bytes from 40.0.0.2 icmp_seq=4 ttl=63 time=18.999 ms
